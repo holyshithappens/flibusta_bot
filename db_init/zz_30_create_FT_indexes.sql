db95fbadebc03ff2c90601f2b97fc34faf701782
harry <holyshithappenstome@gmail.com>
2026-01-25 21:22:16 +0300

    feat: modify zz sql scripts

--- db_init/zz_30_create_FT_indexes.sql
+++ db_init/zz_30_create_FT_indexes.sql
@@ -1,9 +1,9 @@
 -- -- ПОЛНЕТОКСТОВЫЙ ИНДЕКС ДЛЯ ПОИСКА ПО АННОТАЦИИ КНИГ
-CREATE FULLTEXT INDEX idx_annotations_body_ft ON libbannotations (Body);
+CREATE FULLTEXT INDEX IF NOT EXISTS idx_annotations_body_ft ON libbannotations (Body);
 SELECT 'Fulltext index idx_annotations_body_ft created' AS OperationStatus;
 
 -- -- ПОЛНЕТОКСТОВЫЙ ИНДЕКС ДЛЯ ПОИСКА ПО АННОТАЦИИ АВТОРОВ
-CREATE FULLTEXT INDEX idx_author_bio_ft ON libaannotations (Body);
+CREATE FULLTEXT INDEX IF NOT EXISTS idx_author_bio_ft ON libaannotations (Body);
 SELECT 'Fulltext index idx_author_bio_ft created' AS OperationStatus;
 
 SELECT 'All fulltext indexes creation completed' AS OperationStatus;
--- db_init/zz_60_migrate_to_cb_tables.sql
+++ db_init/zz_60_migrate_to_cb_tables.sql
@@ -27,39 +27,39 @@
 SELECT 'Starting migration: lib* tables to cb_lib*' AS OperationStatus;
 
 -- Основные таблицы
-RENAME TABLE libbook TO cb_libbook;
+RENAME TABLE IF EXISTS libbook TO cb_libbook;
 SELECT 'Renamed libbook to cb_libbook' AS OperationStatus;
-RENAME TABLE libavtor TO cb_libavtor;
+RENAME TABLE IF EXISTS libavtor TO cb_libavtor;
 SELECT 'Renamed libavtor to cb_libavtor' AS OperationStatus;
-RENAME TABLE libavtorname TO cb_libavtorname;
+RENAME TABLE IF EXISTS libavtorname TO cb_libavtorname;
 SELECT 'Renamed libavtorname to cb_libavtorname' AS OperationStatus;
-RENAME TABLE libgenre TO cb_libgenre;
+RENAME TABLE IF EXISTS libgenre TO cb_libgenre;
 SELECT 'Renamed libgenre to cb_libgenre' AS OperationStatus;
-RENAME TABLE libgenrelist TO cb_libgenrelist;
+RENAME TABLE IF EXISTS libgenrelist TO cb_libgenrelist;
 SELECT 'Renamed libgenrelist to cb_libgenrelist' AS OperationStatus;
-RENAME TABLE libseq TO cb_libseq;
+RENAME TABLE IF EXISTS libseq TO cb_libseq;
 SELECT 'Renamed libseq to cb_libseq' AS OperationStatus;
-RENAME TABLE libseqname TO cb_libseqname;
+RENAME TABLE IF EXISTS libseqname TO cb_libseqname;
 SELECT 'Renamed libseqname to cb_libseqname' AS OperationStatus;
-RENAME TABLE librate TO cb_librate;
+RENAME TABLE IF EXISTS librate TO cb_librate;
 SELECT 'Renamed librate to cb_librate' AS OperationStatus;
-RENAME TABLE librecs TO cb_librecs;
+RENAME TABLE IF EXISTS librecs TO cb_librecs;
 SELECT 'Renamed librecs to cb_librecs' AS OperationStatus;
-RENAME TABLE libreviews TO cb_libreviews;
+RENAME TABLE IF EXISTS libreviews TO cb_libreviews;
 SELECT 'Renamed libreviews to cb_libreviews' AS OperationStatus;
-RENAME TABLE libapics TO cb_libapics;
+RENAME TABLE IF EXISTS libapics TO cb_libapics;
 SELECT 'Renamed libapics to cb_libapics' AS OperationStatus;
-RENAME TABLE libbpics TO cb_libbpics;
+RENAME TABLE IF EXISTS libbpics TO cb_libbpics;
 SELECT 'Renamed libbpics to cb_libbpics' AS OperationStatus;
 
 -- Аннотации
-RENAME TABLE libbannotations TO cb_libbannotations;
+RENAME TABLE IF EXISTS libbannotations TO cb_libbannotations;
 SELECT 'Renamed libbannotations to cb_libbannotations' AS OperationStatus;
-RENAME TABLE libaannotations TO cb_libaannotations;
+RENAME TABLE IF EXISTS libaannotations TO cb_libaannotations;
 SELECT 'Renamed libaannotations to cb_libaannotations' AS OperationStatus;
 
 -- Полнотекстовый индекс
-RENAME TABLE libbook_fts TO cb_libbook_fts;
+RENAME TABLE IF EXISTS libbook_fts TO cb_libbook_fts;
 SELECT 'Renamed libbook_fts to cb_libbook_fts' AS OperationStatus;
 
 SELECT 'Migration completed: All lib* tables renamed to cb_lib*' AS OperationStatus;
