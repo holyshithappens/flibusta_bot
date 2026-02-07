4fe7551c78db28abaa8ed89dc9661817df734a14
harry <holyshithappenstome@gmail.com>
2026-02-07 18:47:11 +0300

    hotfix: change cleanup interval to 3600secs

--- app/VERSION.py
+++ app/VERSION.py
@@ -6,6 +6,7 @@ __version__ = "1.1.4"
 __version_info__ = (1, 1, 4)
 
 # История версий:
+# 1.1.5 (2026-02-07) - Hotfix cleanup interval to 3600 secs
 # 1.1.4 (2026-02-07) - Invalidate cache if DB updated and hotfix cleanup inactive sessions
 # 1.1.0 (2025-12-14) - Migration to cb_ tables for safe updates
 # 1.0.0 (2025-01-15) - Initial production version
