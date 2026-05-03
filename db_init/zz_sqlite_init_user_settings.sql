f411389f9134135e047cc82374d790ce9ef2f18d
harry <holyshithappenstome@gmail.com>
2026-05-03 21:48:09 +0300

    feat: sqlite ddl for user settings

---
+++ db_init/zz_sqlite_init_user_settings.sql
@@ -0,0 +1,20 @@
+-- UserSettings definition
+
+CREATE TABLE UserSettings (
+                    User_ID INTEGER NOT NULL UNIQUE,
+                    MaxBooks INTEGER NOT NULL DEFAULT 20,
+                    Lang VARCHAR(2) DEFAULT '',
+                    DateSortOrder VARCHAR(10) DEFAULT 'DESC',
+                    BookFormat VARCHAR(5) DEFAULT 'fb2',
+                    LastNewsDate VARCHAR(10) DEFAULT ('2000-01-01'),
+                    IsBlocked BOOLEAN DEFAULT (FALSE),
+                    BookSize TEXT DEFAULT '',
+                    SearchType TEXT DEFAULT 'books',
+                    Rating TEXT DEFAULT '',
+                    SearchArea TEXT DEFAULT 'b',
+                    Locale VARCHAR(5) DEFAULT '',
+                    PRIMARY KEY(User_ID)
+                );
+
+CREATE UNIQUE INDEX IXUserSettings_User_ID 
+                ON UserSettings (User_ID);
