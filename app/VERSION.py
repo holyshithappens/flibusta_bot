07313226b64700339132ab7300fdb96e2053aa3f
harry <holyshithappenstome@gmail.com>
2026-06-11 22:56:34 +0300

    chore: bump version to 1.7.0, update changelog for genre filtering release

--- app/VERSION.py
+++ app/VERSION.py
@@ -2,10 +2,11 @@
 Версия проекта
 """
 
-__version__ = "1.6.0"
-__version_info__ = (1, 6, 0)
+__version__ = "1.7.0"
+__version_info__ = (1, 7, 0)
 
 # История версий:
+# 1.7.0 (2026-06-11) - Feat: add genre filtering in search, author and series views
 # 1.6.0 (2026-06-03) - Docs: update README and spec with recent improvements
 # 1.5.1 (2026-06-02) - Feat: log date of user received broadcast message
 # 1.5.0 (2026-06-02) - Feat: display person_type (author/translator) in UI, fix author/translator info view from book info
